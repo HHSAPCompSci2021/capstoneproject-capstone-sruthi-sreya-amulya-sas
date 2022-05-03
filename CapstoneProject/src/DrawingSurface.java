a4bd2283a40f0113d1271dfa29741fedf3dfe1b7
sruthi putcha <sputcha803@student.fuhsd.org>
2022-05-03 10:36:11 -0700

    main class

---
+++ CapstoneProject/src/DrawingSurface.java
@@ -0,0 +1,11 @@
+import processing.awt.PSurfaceAWT;
+import processing.core.PApplet;
+
+public class DrawingSurface extends PApplet {
+
+	public PSurfaceAWT getSurface() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
