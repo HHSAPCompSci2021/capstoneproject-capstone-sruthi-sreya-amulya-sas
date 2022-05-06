c48e18645b0e9d508b35a6ee6ded7043c0d9a69c
sruthi putcha <sputcha803@student.fuhsd.org>
2022-05-05 17:59:29 -0700

    drawing surface

--- CapstoneProject/src/DrawingSurface.java
+++ CapstoneProject/src/DrawingSurface.java
@@ -1,11 +1,58 @@
+import java.awt.event.KeyEvent;
+
 import processing.awt.PSurfaceAWT;
 import processing.core.PApplet;
+import processing.event.MouseEvent;
 
 public class DrawingSurface extends PApplet {
 
-	public PSurfaceAWT getSurface() {
-		// TODO Auto-generated method stub
-		return null;
+	
+
+	private int level, length;
+	
+	public DrawingSurface() {
+	
+
 	}
+	
+	// The statements in the setup() function 
+	// execute once when the program begins
+	public void setup() {
+		
+	}
+	
+	// The statements in draw() are executed until the 
+	// program is stopped. Each statement is executed in 
+	// sequence and after the last line is read, the first 
+	// line is executed again.
+	public void draw() { 
+		background(255);   // Clear the screen with a white background
+		
+		textSize(12);
+		fill(0);
+			
+	}
+	
+	
+
+	public void mouseWheel(MouseEvent event) {
+		  
+	}
+	
+	public void keyPressed() {
+
+		if (keyCode == KeyEvent.VK_UP) {
+			
+		} 
+		else if (keyCode == KeyEvent.VK_DOWN) {
+			
+		} 
+			else if (keyCode == KeyEvent.VK_LEFT) {
+				
+			} 
+			else if (keyCode == KeyEvent.VK_RIGHT) {
+				
+			}
+	}	
 
 }
