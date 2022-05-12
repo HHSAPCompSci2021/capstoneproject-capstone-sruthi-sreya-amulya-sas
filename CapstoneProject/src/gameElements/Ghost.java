2383bdf88373e1ce1e72c3f590c82ea99ddda7a0
sruthi putcha <sputcha803@student.fuhsd.org>
2022-05-12 12:01:58 -0700

    ghost update

--- CapstoneProject/src/gameElements/Ghost.java
+++ CapstoneProject/src/gameElements/Ghost.java
@@ -1,5 +1,37 @@
 package gameElements;
 
+import processing.core.PApplet;
+import processing.core.PImage;
+
 public class Ghost {
 
+private int x, y;
+	
+	public Ghost (int x1, int y1) {
+		x = x1;
+		y = y1;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public void draw(PApplet p) {
+		PImage img = new PImage();
+		img = p.loadImage("images/coin.gif");
+		img.resize(20, 20);
+		p.image(img, x, y);
+	}
 }
